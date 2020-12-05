c83243a7bec9051f9457f196dd1687aaeda17efc
MayurHpNotebook <001mayur94@gmail.com>
2020-12-05 20:16:14 +0530

    3 tc passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
-// import React from 'react';
-// import "./../styles/App.css";
+import React from 'react';
+import "./../styles/App.css";
 
-// export default class App extends React.Component {
-//     render() {
-//         return <div id="main">
-//             <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-//         </div>
-//     }
-// }
+export default class App extends React.Component {
+    render() {
+        return (<div id="main">
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        </div>);
+    }
+}
 
-const App= <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>;
-export default App;
