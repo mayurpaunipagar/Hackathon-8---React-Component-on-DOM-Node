cfcc0672a6da870bf4af6ff102ccd44294256fb8
MayurHpNotebook <001mayur94@gmail.com>
2020-12-05 17:40:07 +0530

    added code

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,9 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react';
+import "./../styles/App.css";
+
+export default class App extends React.Component{
+    render(){
+        return <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>; 
+    }
+}
 
