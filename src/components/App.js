5f94a77baecc23a8869ac3d7b227dbecb1ab3311
MayurHpNotebook <001mayur94@gmail.com>
2020-12-05 17:59:19 +0530

    added id main

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import "./../styles/App.css";
 
 export default class App extends React.Component {
     render() {
-        return <div>
+        return <div id="main">
             <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
         </div>
     }
