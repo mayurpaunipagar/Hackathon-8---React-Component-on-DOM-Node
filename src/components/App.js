d46cb5a6b93cf087fca7b61c1b56509c3bf70f21
MayurHpNotebook <001mayur94@gmail.com>
2020-12-05 17:53:26 +0530

    changed jsx

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import "./../styles/App.css";
 
-export default class App extends React.Component{
-    render(){
-        return (<>
-        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> 
-        </>);
+export default class App extends React.Component {
+    render() {
+        return <div>
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        </div>
     }
 }
 
