bff1e6d07422edc0728931d2afac9fea1626dc7c
MayurHpNotebook <001mayur94@gmail.com>
2020-12-07 00:15:40 +0530

    only functional component works

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import "./../styles/App.css";
 
-export default class App extends React.Component {
-    render() {
-        return (<div id="main">
-            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-        </div>);
-    }
+import React from "react";
+
+export default function App(){
+    return(
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    );
 }
-
